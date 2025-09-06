50192fc3fd2e43d87f49b8ae2f2a4748afbfd6d5
Santiago Ochoa Paipilla <santiagop200611@gmail.com>
2025-09-06 11:11:49 -0500

    Se realizo el guardado en localStorage

--- js/index.js
+++ js/index.js
@@ -1,8 +1,17 @@
 const form = document.forms['addTask']
+let num = 0
 
 form.addEventListener('submit', (e)=> {
     e.preventDefault()
-    alert('Form submitted')
+    const task = form['taskName'].value
     const date = form['date'].value
     const description = form['description'].value
+    const savetask = {
+        task,
+        date,
+        description
+    }
+    num++
+    localStorage.setItem(`task_${num}`, JSON.stringify(savetask))
+    form.reset()
 })
