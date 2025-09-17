ce825fede127e5527f725fcbbd4871aa6066a38f
Santiago Ochoa Paipilla <santiagop200611@gmail.com>
2025-09-17 15:42:37 -0500

    Se inicia la organizacion del las variables en una solo archivo para mayor legilibilidad

---
+++ js/variable.js
@@ -0,0 +1,11 @@
+let cartas = []
+let primeraCarta = null
+let segundaCarta = null
+let bloqueoTablero = false
+let intentos = 0
+let paresEncontrados = 0
+let totalPares = 0
+let tiempoInicio
+let intervaloTiempo
+let baseDeDatos
+let tablegame = document.getElementById('table')
